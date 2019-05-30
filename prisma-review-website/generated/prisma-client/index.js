823631f53e67b6bbd4e1a683f8a0e350c2a517eb
Nguyen Quang An <ngquangan@gmail.com>
2019-05-30 13:09:13 +0700

    Create review book project with prisma and create datamodel

---
+++ prisma-review-website/generated/prisma-client/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "User",
+    embedded: false
+  },
+  {
+    name: "Book",
+    embedded: false
+  },
+  {
+    name: "Review",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `http://localhost:4466/reviews/default`
+});
+exports.prisma = new exports.Prisma();
