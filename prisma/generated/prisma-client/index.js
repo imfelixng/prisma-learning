fff4e99fd487de4fdc5994f249e4ae12e1c557ce
Nguyen Quang An <ngquangan@gmail.com>
2019-06-04 03:55:10 +0700

    Create secret key for prisma

--- prisma/generated/prisma-client/index.js
+++ prisma/generated/prisma-client/index.js
@@ -20,6 +20,7 @@ var models = [
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
   typeDefs,
   models,
-  endpoint: `http://localhost:4466`
+  endpoint: `http://localhost:4466`,
+  secret: `thisismysupersecret`
 });
 exports.prisma = new exports.Prisma();
