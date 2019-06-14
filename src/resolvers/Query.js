e19015f149005212a20cc6af80a49c6af229db74
Nguyen Quang An <ngquangan@gmail.com>
2019-06-14 13:30:04 +0700

    Add orderBy

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -6,6 +6,7 @@ const Query = {
       first: args.first,
       skip: args.skip,
       after: args.after,
+      orderBy: args.orderBy
     }
 
     if (args.query) {
@@ -24,6 +25,7 @@ const Query = {
       first: args.first,
       skip: args.skip, // skip at next post of after id
       after: args.after,
+      orderBy: args.orderBy,
       where: {
         published: false,
       }
@@ -45,6 +47,7 @@ const Query = {
       first: args.first,
       skip: args.skip,
       after: args.after,
+      orderBy: args.orderBy,
     }
     return prisma.query.comments(opArgs, info);
   },
@@ -83,6 +86,7 @@ const Query = {
       first: args.first,
       skip: args.skip,
       after: args.after,
+      orderBy: args.orderBy,
       where: {
         author: {
           id: userId
