631c4a8cbfbec20c28b38be5e32820eae2a2fa3e
Nguyen Quang An <ngquangan@gmail.com>
2019-06-16 21:51:04 +0700

    Dummy data with beforeEach of jest

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,8 +1,50 @@
 import 'cross-fetch/polyfill';
 import ApolloClient, { gql } from "apollo-boost";
+import bcrypt from 'bcryptjs';
+
+import prisma from '../src/prisma.js';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000'
+});
+
+beforeEach(async () => {
+  await prisma.mutation.deleteManyPosts();
+  await prisma.mutation.deleteManyUsers();
+  const user = await prisma.mutation.createUser({
+    data: {
+      name: "An",
+      email: 'ngquangan.demo@gmail.com',
+      password: bcrypt.hashSync('test123')
+    }
+  })
+
+  await prisma.mutation.createPost({
+    data: {
+      title: 'This is test post',
+      body: 'This is body',
+      published: true,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  })
+
+  await prisma.mutation.createPost({
+    data: {
+      title: 'This is tes t post1',
+      body: 'This is body1',
+      published: false,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  })
+
 });
 
 test("should be create new user", async () => {
@@ -11,7 +53,7 @@ test("should be create new user", async () => {
       createUser(
         data: {
           name: "ngquangan"
-          email: "ngquangan12345678910@gmail.com"
+          email: "ngquangan@gmail.com"
           password: "ngquangan"
         }
       ) {
@@ -27,4 +69,6 @@ test("should be create new user", async () => {
       mutation: createUser
     })
 
+    const exists = await prisma.exists.User({ id: response.data.createUser.user.id });
+    expect(exists).toBe(true);
 });
